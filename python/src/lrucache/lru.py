3f5b84f288e59b75a8ea054091ae64efe3ed5707
Max Weinstein <maxjacob613@gmail.com>
2025-01-14 17:28:30 -0500

    Python Dockerfile and LRU

--- python/src/lrucache/lru.py
+++ python/src/lrucache/lru.py
@@ -1,15 +1,26 @@
 class LRUCache:
     def __init__(self, capacity):
         self.capacity = capacity
+        # DLL of keys, in order of recent reference
         self.cache = []
+        # For now, just a set
         self.lru = {}
+        # self.lru has to be indexed by keys for fast search
+
 
     def refer(self, key):
-        # not present in cache
-
-        # present in cache
-
+        if key not in self.lru:
+            if len(self.cache) >= self.capacity:
+                old_key = self.cache.pop(0)
+                del self.lru[old_key]
+            # Don't use the stored value
+            self.lru[key] = True
+        else:
+            # Possible O(n) step
+            self.cache.remove(key)
         # update reference
+        # Note: MRU goes at the end
+        self.cache.append(key)
 
     def display(self):
         for i in self.cache:
