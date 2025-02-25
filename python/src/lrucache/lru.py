0dcb9036fba5890d29a2fa1e0be9551d67399373
Max Weinstein <maxjacob613@gmail.com>
2025-02-25 02:44:51 -0500

    Cleaning up the python implementation

--- python/src/lrucache/lru.py
+++ python/src/lrucache/lru.py
@@ -1,28 +1,26 @@
 class LRUCache:
     def __init__(self, capacity):
         self.capacity = capacity
-        # DLL of keys, in order of recent reference
-        self.cache = []
-        # For now, just a set
-        self.lru = {}
-        # self.lru has to be indexed by keys for fast search
+        # Least recently used is index 0
+        self.lru = []
+        # The keys in our cache
+        self.cache = set()
 
 
     def refer(self, key):
-        if key not in self.lru:
-            if len(self.cache) >= self.capacity:
-                old_key = self.cache.pop(0)
-                del self.lru[old_key]
+        if key not in self.cache:
+            while len(self.lru) >= self.capacity:
+                old_key = self.lru.pop(0)
+                self.cache.remove(old_key)
             # Don't use the stored value
-            self.lru[key] = True
+            self.cache.add(key)
         else:
             # Possible O(n) step
-            self.cache.remove(key)
+            self.lru.remove(key)
         # update reference
-        # Note: MRU goes at the end
-        self.cache.append(key)
+        self.lru.append(key)
 
     def display(self):
-        for i in self.cache:
+        for i in self.lru:
             print(i,end=' ')
         print('')
