57726d556e1327e07ed03f4defe8550eb4de7e78
Max Weinstein <maxjacob613@gmail.com>
2025-02-25 21:57:23 -0500

    adjusted python code so it matches the order of the others

--- python/src/lrucache/lru.py
+++ python/src/lrucache/lru.py
@@ -1,7 +1,7 @@
 class LRUCache:
     def __init__(self, capacity):
         self.capacity = capacity
-        # Least recently used is index 0
+        # Most recently used is index 0
         self.lru = []
         # The keys in our cache
         self.cache = set()
@@ -10,13 +10,13 @@ class LRUCache:
     def refer(self, key):
         if key not in self.cache:
             while len(self.lru) >= self.capacity:
-                old_key = self.lru.pop(0)
+                old_key = self.lru.pop()
                 self.cache.remove(old_key)
             self.cache.add(key)
-            self.lru.append(key)
+            self.lru.insert(0, key)
         else:
             self.lru.remove(key)
-            self.lru.append(key)
+            self.lru.insert(0, key)
 
     def display(self):
         for i in self.lru:
